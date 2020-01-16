172b3d787c8b071b4876bef832f6f8588aa13d5c
brian-t-krebs <59836349+brian-t-krebs@users.noreply.github.com>
2020-01-16 16:24:28 -0500

    Display is good... add the functionality.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,42 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+//console.log('Ready to Party With Some TypeScript!');
 
+/* const percentages = document.querySelectorAll('.btn-secondary') as NodeListOf<HTMLDivElement>;
+ */
+//1. On-Click you need to change the selection to label class = btn btn-secondary active -- or add the active class to the label
+
+/* let currentButton = 1;
+percentages.forEach(btn => {
+    btn.addEventListener('click', handleClick);
+});
+ */
+
+document.querySelectorAll('.square') as NodeListOf<HTMLDivElement>;
+
+
+
+function check(num: id) {
+    let that = 
+    if (!that.checked) {
+        that.ad
+    }
+
+
+
+}
+
+/* function ifTipChecked(radio) {
+    if (radio.checked && radio.value == window.lastrv) {
+        $(radio).removeAttr('checked');
+        window.lastrv = 0;
+    }
+    else
+        window.lastrv = radio.value; */
+}
+
+/* fucntion handleClick() {
+    const that = this as HTMLDivElement;
+
+    that.classList.add(' active');
+}; */
