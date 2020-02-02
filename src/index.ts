cb53f0aac2bea815646e80e6b9746d4ad5f0b934
brian-t-krebs <59836349+brian-t-krebs@users.noreply.github.com>
2020-02-02 02:22:17 -0500

    bkrebs1 - checked in finished Lab

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,121 @@
 
 import './styles.css';
-//console.log('Ready to Party With Some TypeScript!');
-
-/* const percentages = document.querySelectorAll('.btn-secondary') as NodeListOf<HTMLDivElement>;
- */
-//1. On-Click you need to change the selection to label class = btn btn-secondary active -- or add the active class to the label
-
-/* let currentButton = 1;
-percentages.forEach(btn => {
-    btn.addEventListener('click', handleClick);
-});
- */
-
-document.querySelectorAll('.square') as NodeListOf<HTMLDivElement>;
+// Set Initial Constants
+const defaultTip = '20';
 
 
+const defaultAmt = 0.0;
+// Get Stuff
+const radioButtons: HTMLInputElement[] = [
+    document.getElementById('10') as HTMLInputElement,
+    document.getElementById('15') as HTMLInputElement,
+    document.getElementById('20') as HTMLInputElement
+];
+const moneySpentEntered = document.getElementById('billAmount') as HTMLInputElement;
 
-function check(num: id) {
-    let that = 
-    if (!that.checked) {
-        that.ad
+const spanbillAmount = document.getElementById('li-billAmount') as HTMLInputElement;
+const spantipPercentage = document.getElementById('li-tipPercentage') as HTMLInputElement;
+const spanamountOfTip = document.getElementById('li-amountOfTip') as HTMLInputElement;
+const spantotalToBePaid = document.getElementById('li-totalToBePaid') as HTMLInputElement;
+
+document.getElementById('percentTip').innerHTML = (defaultTip.toString() + '%');
+document.getElementById('li-tipPercentage').innerHTML = (defaultTip.toString());
+document.getElementById('billAmount').innerText = (defaultTip.toString());
+
+let currentRate = 20;
+// Calculate the Tip itself by listening to the input, for each keystroke // we will repaint after each keystroke.
+// validate it is a currency number too!
+
+
+////////////////////////////////// UPDATE AMOUNTS DISPLAYED AND DO MATH //////////////////////////////////////
+
+// Event Handler!! for keystrokes on input field
+moneySpentEntered.addEventListener('keyup', processUpdatedBillAmount);
+
+// Update Bill Amount Function
+function processUpdatedBillAmount() {
+    // Set Bill Amount.
+    // yeah, if it's emptied, make sure we show zeros.
+    if (moneySpentEntered.value === '') {
+        spanbillAmount.innerText = '0.00';
+        spanamountOfTip.innerText = '0.00';
+        spantotalToBePaid.innerText = '0.00';
+    } else {
+        const amountText = moneySpentEntered.value;
+        const amount = parseFloat(moneySpentEntered.value);
+        // Calc Amt of bill
+        const bill = (amount < 0) ? 0.0 : amount;
+        spanbillAmount.innerText = bill.toFixed(2);
+        // Calc Amt. of tip using currentRate
+        const tip = bill * currentRate / 100.0;
+        spanamountOfTip.innerText = tip.toFixed(2);
+        // calc and update total
+        const totalToBePaid = bill + tip;
+        spantotalToBePaid.innerText = totalToBePaid.toFixed(2);
     }
-
-
-
 }
 
-/* function ifTipChecked(radio) {
-    if (radio.checked && radio.value == window.lastrv) {
-        $(radio).removeAttr('checked');
-        window.lastrv = 0;
-    }
-    else
-        window.lastrv = radio.value; */
+////////////////////////////////// RADIO BUTTON SECTION //////////////////////////////////////
+// Update Tip Radio Buttons
+updateTipAmt(defaultTip);
+
+// Event Handler for Radio Buttons
+radioButtons.forEach(btn => btn.addEventListener('click', handleTipClick));
+
+function handleTipClick() {
+    const radioButtonId = this.id;
+    updateTipAmt(radioButtonId);
+    // Update Displays
+    document.getElementById('percentTip').innerHTML = (radioButtonId.toString() + '%');
+    document.getElementById('li-tipPercentage').innerHTML = (radioButtonId.toString());
 }
 
-/* fucntion handleClick() {
-    const that = this as HTMLDivElement;
-
-    that.classList.add(' active');
-}; */
+// Function for Updating Radio Buttons
+function updateTipAmt(selectedRadioButton: string) {
+    switch (selectedRadioButton) {
+        case '10': {
+            // Update the Radio Button
+            document.getElementById(selectedRadioButton).setAttribute('checked', '');
+            document.getElementById('15').removeAttribute('checked');
+            document.getElementById('20').removeAttribute('checked');
+            if (!document.getElementById('lbl' + selectedRadioButton.toString()).classList.contains('active') === true) {
+            document.getElementById('lbl' + selectedRadioButton.toString()).classList.add('active');
+            }
+            document.getElementById('lbl15').classList.remove('active');
+            document.getElementById('lbl20').classList.remove('active');
+            currentRate = 10;
+            break;
+        }
+        case '15': {
+            // Update the Radio Button
+            document.getElementById(selectedRadioButton).setAttribute('checked', '');
+            document.getElementById('10').removeAttribute('checked');
+            document.getElementById('20').removeAttribute('checked');
+            // Update te Visual Button
+            if (!document.getElementById('lbl' + selectedRadioButton.toString()).classList.contains('active') === true) {
+                document.getElementById('lbl' + selectedRadioButton.toString()).classList.add('active');
+            }
+            document.getElementById('lbl10').classList.remove('active');
+            document.getElementById('lbl20').classList.remove('active');
+            currentRate = 15;
+            break;
+        }
+        case '20': {
+            // Update the Radio Button
+            document.getElementById(selectedRadioButton).setAttribute('checked', '');
+            document.getElementById('10').removeAttribute('checked');
+            document.getElementById('15').removeAttribute('checked');
+            // Update Visual Button via List Item
+            if (!document.getElementById('lbl' + selectedRadioButton.toString()).classList.contains('active') === true) {
+                document.getElementById('lbl' + selectedRadioButton.toString()).classList.add('active');
+            }
+            document.getElementById('lbl10').classList.remove('active');
+            document.getElementById('lbl15').classList.remove('active');
+            currentRate = 20;
+            break;
+            }
+        default: {
+            break;
+        }
+    }
+}
